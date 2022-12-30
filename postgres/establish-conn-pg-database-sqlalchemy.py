e855a8bbfa4d4f3779169fbec46d4548da4adc26
jewelski <jelleeggink@gmail.com>
2022-12-30 21:07:58 +0100

    rename variable

--- postgres/establish-conn-pg-database-sqlalchemy.py
+++ postgres/establish-conn-pg-database-sqlalchemy.py
@@ -2,7 +2,7 @@ import sqlalchemy
 
 # Create a connection string in the format:
 # postgresql://user:password@host:port/database
-conn_string = "postgresql://user:password@host:port/database"
+conn = "postgresql://user:password@host:port/database"
 
 # Create an engine using the connection string
-engine = sqlalchemy.create_engine(conn_string)
+engine = sqlalchemy.create_engine(conn)
